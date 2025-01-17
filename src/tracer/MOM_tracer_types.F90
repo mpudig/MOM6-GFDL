8b4c4f4804ebf1589956291db70b76e9a495669a
mpudig <mattpudig1998@gmail.com>
2025-01-17 14:17:56 +1100

    Add id and diagnostic array for thickness multiplied tracer concentration.

--- src/tracer/MOM_tracer_types.F90
+++ src/tracer/MOM_tracer_types.F90
@@ -15,6 +15,8 @@ type, public :: tracer_type
 ! real, dimension(:,:,:), pointer :: OBC_in_v       => NULL() !< structured values for flow into the domain
 !                                                             !! specified in OBCs through v-face of cell
 
+  real, dimension(:,:,:), pointer :: t_h            => NULL() !< diagnostic array for thickness multipled tracer
+                                                              !! concentration [CU H ~> conc m or conc kg m-2]
   real, dimension(:,:,:), pointer :: ad_x           => NULL() !< diagnostic array for x-advective tracer flux
                                                               !! [CU H L2 T-1 ~> conc m3 s-1 or conc kg s-1]
   real, dimension(:,:,:), pointer :: ad_y           => NULL() !< diagnostic array for y-advective tracer flux
@@ -103,6 +105,7 @@ type, public :: tracer_type
   integer :: diag_form = 1  !< An integer indicating which template is to be used to label diagnostics.
   !>@{ Diagnostic IDs
   integer :: id_tr = -1, id_tr_post_horzn = -1
+  integer :: id_tr_h = -1
   integer :: id_adx = -1, id_ady = -1, id_dfx = -1, id_dfy = -1
   integer :: id_hbd_dfx = -1, id_hbd_dfy = -1
   integer :: id_hbd_dfx_2d = -1, id_hbd_dfy_2d = -1
