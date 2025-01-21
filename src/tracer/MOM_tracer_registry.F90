a3362bd935c2d23b35f4dcd1f587d6553659271e
mpudig <mattpudig1998@gmail.com>
2025-01-21 17:44:54 -0500

    Fix typo, and make changes based on initial errors from compiliation.

--- src/tracer/MOM_tracer_registry.F90
+++ src/tracer/MOM_tracer_registry.F90
@@ -81,8 +81,8 @@ subroutine register_tracer(tr_ptr, Reg, param_file, HI, GV, name, longname, unit
                                                                 !! tracer cells [CU ~> conc]
 
   ! The following are probably not necessary if registry_diags is present and true.
-  real, dimension(:,:,:), optional, pointer     :: t_h          !< diagnostic thickness multiplied tracer
-                                                                !! concentration [CU H ~> conc m or conc kg m-2]
+  ! real, dimension(:,:,:), optional, pointer     :: t_h          !< diagnostic thickness multiplied tracer
+  !                                                              !! concentration [CU H ~> conc m or conc kg m-2]
   real, dimension(:,:,:), optional, pointer     :: ad_x         !< diagnostic x-advective flux
                                                                 !! [CU H L2 T-1 ~> conc m3 s-1 or conc kg s-1]
   real, dimension(:,:,:), optional, pointer     :: ad_y         !< diagnostic y-advective flux
@@ -235,7 +235,7 @@ subroutine register_tracer(tr_ptr, Reg, param_file, HI, GV, name, longname, unit
 
   if (present(registry_diags)) Tr%registry_diags = registry_diags
 
-  if (present(t_h)) then ; if (associated(t_h)) Tr%t_h => t_h ; endif
+!  if (present(t_h)) then ; if (associated(t_h)) Tr%t_h => t_h ; endif
   if (present(ad_x)) then ; if (associated(ad_x)) Tr%ad_x => ad_x ; endif
   if (present(ad_y)) then ; if (associated(ad_y)) Tr%ad_y => ad_y ; endif
   if (present(df_x)) then ; if (associated(df_x)) Tr%df_x => df_x ; endif
@@ -350,7 +350,7 @@ subroutine register_tracer_diagnostics(Reg, h, Time, diag, G, GV, US, use_ALE, u
     Tr%id_tr_h = register_diag_field("ocean_model", &
         trim(name)//"h", diag%axesTL, Time, &
         trim(longname)//" multiplied thickness", &
-        trim(units)" m", conversion=Tr%conc_scale*US%L_to_m)
+        trim(units)//" m", conversion=Tr%conc_scale*US%L_to_m)
     if (Tr%diag_form == 1) then
       Tr%id_adx = register_diag_field("ocean_model", trim(shortnm)//"_adx", &
           diag%axesCuL, Time, trim(flux_longname)//" advective zonal flux" , &
