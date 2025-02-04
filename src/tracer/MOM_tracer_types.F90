446eb3dfdfe44d0d0bfd4bd7cb3590e6fc539f5a
mpudig <mattpudig1998@gmail.com>
2025-02-04 15:52:10 -0500

    Fix typo.

--- src/tracer/MOM_tracer_types.F90
+++ src/tracer/MOM_tracer_types.F90
@@ -105,7 +105,7 @@ type, public :: tracer_type
   integer :: diag_form = 1  !< An integer indicating which template is to be used to label diagnostics.
   !>@{ Diagnostic IDs
   integer :: id_tr = -1, id_tr_post_horzn = -1
-  integer :: id_tr_h = -1,
+  integer :: id_tr_h = -1
   integer :: id_adx = -1, id_ady = -1, id_dfx = -1, id_dfy = -1
   integer :: id_hbd_dfx = -1, id_hbd_dfy = -1
   integer :: id_hbd_dfx_2d = -1, id_hbd_dfy_2d = -1
